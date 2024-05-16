72d4df53630e5daf7f7b453cc38cdd7234c4beda
Agris <agris.magdalenoks@gmail.com>
2024-05-16 23:03:14 +0300

    adds some sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <h1>CV Builder</h1>
+      /** Select for sections to include could use a checkbox section*/
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="checkbox" name="section" value="Personal Details" />
+        <label>Personal Details</label>
+        <input
+          type="checkbox"
+          name="section"
+          value="Education and Qualifications"
+        />
+        <label>Education and Qualifications</label>
+        <input type="checkbox" name="section" value="EMPLOYMENT HISTORY" />
+        <label>EMPLOYMENT HISTORY</label>
+        <input type="checkbox" name="section" value="CLINICAL EXPERIENCE" />
+        <label>CLINICAL EXPERIENCE</label>
+        <input type="checkbox" name="section" value="CERTIFICATION" />
+        <label>CERTIFICATION</label>
+        <input type="checkbox" name="section" value="ACHIEVEMENTS" />
+        <label>ACHIEVEMENTS</label>
+        <input type="checkbox" name="section" value="HOBBIES" />
+        <label>HOBBIES</label>
+        <input type="checkbox" name="section" value="REFERENCES" />
+        <label>REFERENCES</label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Personal Details</div>
+      <div>Education and Qualifications</div>
+      <div>EMPLOYMENT HISTORY</div>
+      <div>CLINICAL EXPERIENCE</div>
+      <div>CERTIFICATION</div>
+      <div>ACHIEVEMENTS</div>
+      <div>HOBBIES</div>
+      <div>REFERENCES</div>
+      /** select for file format */
+      <select>
+        <option value="pdf">PDF</option>
+        <option value="doc">DOC</option>
+      </select>
+      /** ok <button>Print</button> */
     </>
-  )
+  );
 }
 
-export default App
+export default App;
